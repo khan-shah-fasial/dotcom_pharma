e7518cced9d8fbb4d58565c8605c70826d4ee746
Shadab Siddiqui <shadab.siddiqui569@gmail.com>
2025-11-27 16:42:54 +0530

    fix lising page double options

--- resources/views/frontend/product_listing_ajax.blade.php
+++ resources/views/frontend/product_listing_ajax.blade.php
@@ -132,21 +132,6 @@
             'selected_color' => $color ?? null,
           ])
         </div>
-
-        {{-- ATTRIBUTES FILTER COMPONENT --}}
-        <div id="attributes-filter">
-          @include('frontend.'.get_setting('homepage_select').'.partials.filters.attributes_filter', [
-            'attributes' => $attributes,
-            'selected_attribute_values' => $selected_attribute_values ?? [],
-            'is_mobile' => false,
-          ])
-        </div>
-
-        {{-- COLOR FILTER (AJAX-replaceable) --}}
-        @include('frontend.'.get_setting('homepage_select').'.partials.filters.color_filter', [
-          'colors'         => $colors,
-          'selected_color' => $color ?? null,
-        ])
       </div>
       
       <!-- Products Section -->
